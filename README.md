3e0d6853a260c92e2e04b9375fc6083ca1295e22
LisadiKaprio <lisadikaprio@gmail.com>
2024-08-29 21:35:26 +0200

    expanding chat according to book (unbounded channel, broker); resources in readme

--- README.md
+++ README.md
@@ -11,4 +11,26 @@ https://rust-exercises.ferrous-systems.com/latest/book/async-chat/
 
 more stuff to add to first chat exercise
 
+# Useful Links
+[ONLINE PLAYGROUND](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021)
+
+# Short explanation videos
+- [8 deadly mistakes beginner Rust developers make](https://www.youtube.com/watch?v=PbR4ECFIckg)
+
+- [How to fight Rust's borrow checker... and win.](https://www.youtube.com/watch?v=Pg07HQJ0tvI)
+
+- [All Rust string types explained](https://www.youtube.com/watch?v=CpvzeyzgQdw)
+
+- [but what is 'a lifetime?](https://www.youtube.com/watch?v=gRAVZv7V91Q)
+
+- [Stack vs Heap Memory - Simple Explanation](https://www.youtube.com/watch?v=5OJRqkYbK-4)
+
+- [you will never ask about pointers again after watching this video](https://www.youtube.com/watch?v=2ybLD6_2gKM)
+
+```rust
+let x = 5;
+let y = &x; // y is a reference set to the address to x
+let z = *y; // z is the thing pointed to by y, so same as z = 5
+``` 
+
 
--- rust-chat-example-book/src/main.rs
+++ rust-chat-example-book/src/main.rs
@@ -1,92 +1,194 @@
-use std::net::SocketAddr;
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    future::Future,
+    net::SocketAddr,
+};
 
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::{tcp::WriteHalf, TcpListener, TcpStream, ToSocketAddrs},
-    sync::broadcast::{self, error::RecvError, Receiver, Sender},
+    net::{tcp::OwnedWriteHalf, TcpListener, TcpStream, ToSocketAddrs},
+    sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+    task::JoinHandle,
 };
 
+type BoxedResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+fn spawn_and_log_error<F>(function: F) -> JoinHandle<()>
+where
+    F: Future<Output = BoxedResult<()>> + Send + 'static,
+{
+    tokio::spawn(async move {
+        if let Err(e) = function.await {
+            eprintln!("{}", e)
+        }
+    })
+}
+
+// NOTE:    the code from the book implemented here
+//          assumes that you write messages formatted like this:
+//          "other_user_1, other_user_2: Hello world!"
+
 #[tokio::main]
-pub(crate) async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+pub(crate) async fn main() -> BoxedResult<()> {
     accept_loop("127.0.0.1:8080").await
 }
 
-async fn accept_loop(
-    addr: impl ToSocketAddrs,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn accept_loop(addr: impl ToSocketAddrs) -> BoxedResult<()> {
     let listener = TcpListener::bind(addr).await?;
-
-    let (tx, _rx) = broadcast::channel(10);
-
     println!("Connected to {}", listener.local_addr()?.to_string());
 
+    let (broker_sender, broker_receiver) = unbounded_channel();
+    let _broker = tokio::spawn(broker_loop(broker_receiver));
+
+    while let Ok((stream, addr)) = listener.accept().await {
+        println!("Client joined...");
+        spawn_and_log_error(connection_loop(broker_sender.clone(), stream));
+    }
+    Ok(())
+}
+enum Event {
+    NewClient {
+        name: String,
+        write_half: OwnedWriteHalf,
+    },
+    Message {
+        from: String,
+        to: Vec<String>,
+        msg: String,
+    },
+}
+
+async fn broker_loop(mut events: UnboundedReceiver<Event>) {
+    let mut clients: HashMap<String, UnboundedSender<String>> = HashMap::new();
+
     loop {
-        handle_client(&listener, tx.clone()).await;
+        let event = match events.recv().await {
+            Some(event) => event,
+            None => break,
+        };
+
+        match event {
+            Event::NewClient {
+                name,
+                mut write_half,
+            } => match clients.entry(name.clone()) {
+                Entry::Occupied(_) => (),
+                Entry::Vacant(entry) => {
+                    let (client_sender, mut client_receiver) = unbounded_channel();
+                    entry.insert(client_sender);
+                    spawn_and_log_error(async move {
+                        receive_messages_on_loop(&mut client_receiver, &mut write_half).await
+                    });
+                }
+            },
+            Event::Message { from, to, msg } => {
+                for addr in to {
+                    if let Some(client) = clients.get(&addr) {
+                        let message = format!("{:?}: {:?}\n", from, &msg);
+                        let sending_attempt = client.send(message.clone());
+                        if let Err(e) = sending_attempt {
+                            eprintln!(
+                                "Error while receiving message from {from} on {:?}: {e}",
+                                &addr
+                            );
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
-async fn handle_client(
-    listener: &TcpListener,
-    tx: Sender<(String, SocketAddr)>,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let mut rx = tx.subscribe();
-    let (mut stream, addr) = listener.accept().await.unwrap();
-    println!("Client joined...");
-
-    tokio::spawn(async move {
-        handle_communication(&mut stream, tx.clone(), &mut rx, &addr).await;
-    });
-    Ok(())
-}
-
-async fn handle_communication(
-    stream: &mut TcpStream,
-    tx: Sender<(String, SocketAddr)>,
-    rx: &mut Receiver<(String, SocketAddr)>,
-    addr: &SocketAddr,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let (read_half, mut write_half) = stream.split();
-    let mut reader = BufReader::new(read_half);
-
-    write_half
-        .write_all(&"Input your name: ".as_bytes())
-        .await
-        .unwrap();
-    let mut name = String::new();
-    reader.read_line(&mut name).await?;
-    // let name = match first_line {
-    //     None => Err("peer disconnected immediately")?,
-    //     Some(line) => line,
-    // };
-    println!("name = {}", name);
-
-    let mut line = String::new();
-
+async fn receive_messages_on_loop(
+    client_receiver: &mut UnboundedReceiver<String>,
+    write_half: &mut OwnedWriteHalf,
+) -> BoxedResult<()> {
     loop {
-        tokio::select! {
-            _result = reader.read_line(&mut line) => {
-                tx.send((line.clone(), addr.clone())).unwrap();
-                line.clear();
-            }
-            result = rx.recv() => {
-                receive_message(result, &mut write_half, &mut line, addr).await?
-            }
+        let message = client_receiver.recv().await;
+        match message {
+            Some(message) => write_half.write_all(&message.as_bytes()).await?,
+            None => break,
         }
     }
     Ok(())
 }
 
-async fn receive_message(
-    result: Result<(String, SocketAddr), RecvError>,
-    write_half: &mut WriteHalf<'_>,
-    line: &mut String,
-    addr: &SocketAddr,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let (msg, other_addr) = result.unwrap();
-
-    if addr != &other_addr {
-        write_half.write_all(&msg.as_bytes()).await.unwrap();
-    }
-    line.clear();
+async fn connection_loop(
+    broker_sender: UnboundedSender<Event>,
+    stream: TcpStream,
+) -> BoxedResult<()> {
+    tokio::spawn(async move {
+        let communication = handle_communication(broker_sender.clone(), stream).await;
+        if let Err(e) = communication {
+            eprintln!("Error on handling communication: {e}");
+        }
+    });
     Ok(())
 }
+
+async fn handle_communication(
+    broker_sender: UnboundedSender<Event>,
+    stream: TcpStream,
+) -> BoxedResult<()> {
+    let (read_half, mut write_half) = stream.into_split();
+    let reader = BufReader::new(read_half);
+    let mut lines = reader.lines();
+
+    write_half
+        .write_all(&"Input your name: ".as_bytes())
+        .await
+        .unwrap();
+    let name = match lines.next_line().await? {
+        None => Err("peer disconnected immediately")?,
+        Some(line) => line,
+    };
+    println!("{} joined.", name);
+
+    // deliberate unwrap() for broker actions, as mentioned in book
+    broker_sender
+        .send(Event::NewClient {
+            name: name.clone(),
+            write_half,
+        })
+        .unwrap();
+
+    loop {
+        if let Some(line) = lines.next_line().await? {
+            println!("{:?}", &line);
+            let (dest, message) = match line.find(':') {
+                None => continue,
+                Some(idx) => (&line[..idx], line[idx + 1..].trim()),
+            };
+            let dest: Vec<String> = dest
+                .split(',')
+                .map(|name| name.trim().to_string())
+                .collect();
+            let message = message.trim().to_string();
+
+            // deliberate unwrap() for broker actions, as mentioned in book
+            broker_sender
+                .send(Event::Message {
+                    from: name.clone(),
+                    to: dest,
+                    msg: message,
+                })
+                .unwrap();
+        } else {
+            break;
+        }
+    }
+    Ok(())
+}
+
+// async fn receive_message(
+//     result: Result<(String, SocketAddr), RecvError>,
+//     write_half: &mut WriteHalf<'_>,
+//     addr: &SocketAddr,
+// ) -> BoxedResult<()> {
+//     let (msg, other_addr) = result.unwrap();
+
+//     if addr != &other_addr {
+//         write_half.write_all(&msg.as_bytes()).await?;
+//     }
+//     Ok(())
+// }
